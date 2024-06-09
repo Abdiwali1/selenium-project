2fa075111314a00f5d3cc7dd4df0cc55e653c953
adamar <aysun114@hotmail.com>
2024-06-09 14:05:59 -0500

    RegistrationFormPage added

--- src/test/java/com/cydeo/tests/review/day4_5_6/RegistrationFormPage.java
+++ src/test/java/com/cydeo/tests/review/day4_5_6/RegistrationFormPage.java
@@ -3,16 +3,31 @@ package com.cydeo.tests.review.day4_5_6;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class RegistrationFormPage {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        // 1. Open Chrome browser
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
 
     @Test
     public void registration_form_page_title(){
 
         // 1. Open Chrome browser
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+//        WebDriver driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         // 2. Go to https://practice.cydeo.com/registration_form
          driver.get("https://practice.cydeo.com/registration_form");
@@ -25,6 +40,31 @@ public class RegistrationFormPage {
         Assert.assertEquals(actualTitle,expectedTitle,"Title verification is failed!");
 
     //    Assert.assertTrue(actualTitle.equals(expectedTitle),"Title verification is failed!");
+
+    }
+
+    @Test
+    public void registration_form_filling_out_test(){
+        // 1. Open Chrome browser
+//        WebDriver driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // 2. Go to https://practice.cydeo.com/registration_form
+        // 3. Enter First name: "John"
+        // 4. Enter Last name: "Smith"
+        // 5. Enter Username: "johnsmith123"
+        // 6. Enter Email address: "john.smith@email.com"
+        // 7. Enter password: "John1234"
+        // 8. Enter Phone number: "123-456-7890"
+        // 9. Click to "Male" from Gender
+        // 10. Enter Date of birth "01/28/1990"
+        // 11. Select "Department of Engineering" from Department/Office dropdown
+        // 12. Select "SDET" from Job title dropdown
+        // 13. Click to "Java" from languages
+        // 14. Click to "Sign up" button
+        // 15. Verify text displayed on page
+        //     Expected: "Well done!"
 
 
     }
