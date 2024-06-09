873fa6204922621b09e1a5934a1f3e54b5b17fde
adamar <aysun114@hotmail.com>
2024-06-09 14:57:33 -0500

    RegistrationFormPage added

--- src/test/java/com/cydeo/tests/review/day4_5_6/RegistrationFormPage.java
+++ src/test/java/com/cydeo/tests/review/day4_5_6/RegistrationFormPage.java
@@ -1,9 +1,11 @@
 package com.cydeo.tests.review.day4_5_6;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -52,14 +54,30 @@ public class RegistrationFormPage {
 //        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         // 2. Go to https://practice.cydeo.com/registration_form
+         driver.get("https://practice.cydeo.com/registration_form");
+
         // 3. Enter First name: "John"
+        driver.findElement(By.name("firstname")).sendKeys("John");
+
         // 4. Enter Last name: "Smith"
+        driver.findElement(By.name("lastname")).sendKeys("Smith");
+
         // 5. Enter Username: "johnsmith123"
+        driver.findElement(By.xpath("(//input[@class='form-control'])[3]")).sendKeys("johnsmith123");
+
         // 6. Enter Email address: "john.smith@email.com"
+        driver.findElement(By.cssSelector("input[placeholder='email@email.com']")).sendKeys("john.smith@email.com");
+
         // 7. Enter password: "John1234"
         // 8. Enter Phone number: "123-456-7890"
+
+
         // 9. Click to "Male" from Gender
+
+
         // 10. Enter Date of birth "01/28/1990"
+
+
         // 11. Select "Department of Engineering" from Department/Office dropdown
         // 12. Select "SDET" from Job title dropdown
         // 13. Click to "Java" from languages
@@ -68,5 +86,10 @@ public class RegistrationFormPage {
         //     Expected: "Well done!"
 
 
+    }
+
+    @AfterMethod
+    public void teardown(){
+     //   driver.quit();
     }
 }
