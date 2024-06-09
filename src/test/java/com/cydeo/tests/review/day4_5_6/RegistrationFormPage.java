6a4070876d8925d7dbdfebdf406a235568bb1a77
adamar <aysun114@hotmail.com>
2024-06-09 16:03:49 -0500

    RegistrationFormPage added

--- src/test/java/com/cydeo/tests/review/day4_5_6/RegistrationFormPage.java
+++ src/test/java/com/cydeo/tests/review/day4_5_6/RegistrationFormPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -25,7 +26,7 @@ public class RegistrationFormPage {
     }
 
 
-    @Test
+    @Test(priority = 1)
     public void registration_form_page_title(){
 
         // 1. Open Chrome browser
@@ -47,7 +48,7 @@ public class RegistrationFormPage {
 
     }
 
-    @Test
+    @Test(priority = 2)
     public void registration_form_filling_out_test(){
         // 1. Open Chrome browser
 //        WebDriver driver = new ChromeDriver();
@@ -73,15 +74,29 @@ public class RegistrationFormPage {
         // 8. Enter Phone number: "123-456-7890"
 
 
+
         // 9. Click to "Male" from Gender
         WebElement maleBtn = driver.findElement(By.cssSelector("input[value='male']"));
         maleBtn.click();
         Assert.assertTrue(maleBtn.isSelected(),"Male button is not selected!");
 
+
         // 10. Enter Date of birth "01/28/1990"
 
 
         // 11. Select "Department of Engineering" from Department/Office dropdown
+        Select selectDepartment = new Select(driver.findElement(By.name("department")));
+        System.out.println("selectDepartment.getFirstSelectedOption() = " + selectDepartment.getFirstSelectedOption().getText());
+
+//        selectDepartment.selectByIndex(1);
+//        selectDepartment.selectByValue("DE");
+        selectDepartment.selectByVisibleText("Department of Engineering");
+
+        String actualEngineeringOption = selectDepartment.getFirstSelectedOption().getText();
+        String expectedEngineeringOption = "Department of Engineering";
+
+        Assert.assertEquals(actualEngineeringOption,expectedEngineeringOption);
+
         // 12. Select "SDET" from Job title dropdown
         // 13. Click to "Java" from languages
         // 14. Click to "Sign up" button
