4c43e0a37fafe31030b1f1b60ec5cad39a1deaf7
adamar <aysun114@hotmail.com>
2024-06-15 09:49:28 -0500

    TestBase added

---
+++ src/test/java/com/cydeo/tests/review/day7_8_9/IframePractice.java
@@ -0,0 +1,32 @@
+package com.cydeo.tests.review.day7_8_9;
+
+import com.cydeo.tests.base.TestBase;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class IframePractice extends TestBase {
+
+    @Test
+    public void iframe_test(){
+        // 1. Users are already login and on the homepage of CRM app
+        driver.get();
+
+        // 2. Users click the MESSAGE tab
+
+
+        // 3. Users write test message
+
+
+        // 4. Users click the SEND button
+
+
+        // 5. Verify the message is displayed on the feed
+
+    }
+
+}
