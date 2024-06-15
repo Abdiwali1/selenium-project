118dd90fbf0b6277a70f2d2fd23ecbbcbcec5501
adamar <aysun114@hotmail.com>
2024-06-15 10:44:19 -0500

    IframePractice added

--- src/test/java/com/cydeo/tests/review/day7_8_9/IframePractice.java
+++ src/test/java/com/cydeo/tests/review/day7_8_9/IframePractice.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.review.day7_8_9;
 
 import com.cydeo.tests.base.TestBase;
+import com.cydeo.utilities.CRM_Utilities;
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -14,16 +16,24 @@ public class IframePractice extends TestBase {
     @Test
     public void iframe_test(){
         // 1. Users are already login and on the homepage of CRM app
-        driver.get();
+        driver.get("https://login1.nextbasecrm.com/");
+
+        CRM_Utilities.login_crm(driver);
 
         // 2. Users click the MESSAGE tab
-
+        driver.findElement(By.id("feed-add-post-form-tab-message")).click();
 
         // 3. Users write test message
+     //   driver.switchTo().frame(0);
+        driver.switchTo().frame(driver.findElement(By.className("bx-editor-iframe")));
 
+        driver.findElement(By.tagName("body")).sendKeys("whatever");
 
         // 4. Users click the SEND button
+        driver.switchTo().defaultContent(); // If you want to go back to main HTML
+    //    driver.switchTo().parentFrame(); // If iframe is nested child iframe and you want to go back to parent iframe
 
+        driver.findElement(By.id("blog-submit-button-save")).click();
 
         // 5. Verify the message is displayed on the feed
 
