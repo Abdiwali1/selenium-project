1cd4e4ef911ef4dc05edd8cd7a5c4bee05959c0b
adamar <aysun114@hotmail.com>
2024-06-15 09:27:02 -0500

    AlertPractice

---
+++ src/test/java/com/cydeo/tests/review/day7_8_9/AlertPractice.java
@@ -0,0 +1,55 @@
+package com.cydeo.tests.review.day7_8_9;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class AlertPractice {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        // 1. Open Chrome browser
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void alert_test(){
+        // 2. Go to website: https://practice.cydeo.com/javascript_alerts
+        driver.get("https://practice.cydeo.com/javascript_alerts");
+
+        // 3. Click to “Click for JS Prompt” button
+        driver.findElement(By.xpath("//button[.='Click for JS Prompt']")).click();
+
+        // 4. Send “hello” text to alert
+        Alert alert = driver.switchTo().alert();
+
+        alert.sendKeys("hello");
+
+        // 5. Click to OK button from the alert
+        alert.accept();
+
+        // 6. Verify “You entered: hello” text is displayed.
+        WebElement resultText = driver.findElement(By.id("result"));
+
+        Assert.assertEquals(resultText.getText(),"You entered: hello");
+
+    }
+
+
+    @AfterMethod
+    public void teardown(){
+     //   driver.quit();
+    }
+}
